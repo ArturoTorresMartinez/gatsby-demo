2515fc4633f9eeebdc4b97dba678496c7306011e
ArturoTorres <arturo.torres@not-me.com>
2021-03-10 14:09:02 -0600

    Add support for Bootstrap and Sass
    This adds Bootstrap 4 as Boostrap 5 is on beta and SASS support,
    added a scss file for styling for index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,29 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import "../styles/index.scss"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  return(
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)}
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="container-fluid">
+        <div className="row MainRow">
+          <div className="col">
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <p>
+              <Link to="/page-2/">Go to page 2</Link> <br />
+              <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage

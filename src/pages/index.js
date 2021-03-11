65fd69b739040a24490528dc11ecfab70583a42a
ArturoTorres <arturo.torres@not-me.com>
2021-03-10 21:56:31 -0600

    Remove unnecessary links from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import "../styles/index.scss"
 
 import Layout from "../components/layout"
@@ -79,10 +79,6 @@ const IndexPage = () => {
             <ul className="List">
               <RenderItems data={repositories} />
             </ul>
-            <p>
-              <Link to="/page-2/">Go to page 2</Link> <br />
-              <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-            </p>
           </div>
         </div>
       </div>
